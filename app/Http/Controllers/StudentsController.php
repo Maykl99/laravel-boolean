cc33d7b1bfb63f4a8f2d8f779d75f91070d4c34e
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-10-13 17:57:05 +0200

    pagine interne metodi

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -7,8 +7,22 @@ use Illuminate\Http\Request;
 #estendo controller, le mie funzioni ritornano le view, dopo aver superato eventuali controlli, logica del controller
 class StudentsController extends Controller
 {
-    public function studenti(){
+
+    protected $students;
+
+    public function __construct()
+    {
+        $this->students;
+    }
+
+    public function studenti()
+    {
         $data=config('students'); # dati presi dalla cartella config, nome cartella 'students'
         return view('studenti',compact('data')); #['data' => $data]
+    }
+
+    public function show()
+    {
+        
     }
 }
